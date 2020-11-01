8922dcb38c7a6b0e9cbdb02a70036d7bef207b51
Jakob <jakob@folkdatorn.se>
2020-11-01 17:24:29 +0100

    v1

---
+++ docs/precache-manifest.911ebe32f1cad83c8610a586ea6e7982.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7bdf417fdc224bf679f97cc36e4a23b",
+    "url": "/smallimprint-stock-chart-demo/index.html"
+  },
+  {
+    "revision": "a56bad5c3ac6165fcc77",
+    "url": "/smallimprint-stock-chart-demo/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "b7b3150996bc8b41e2a1",
+    "url": "/smallimprint-stock-chart-demo/static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "a56bad5c3ac6165fcc77",
+    "url": "/smallimprint-stock-chart-demo/static/js/2.e40119f0.chunk.js"
+  },
+  {
+    "revision": "9b2acb6a75586109f6d6ad7309e7fe5d",
+    "url": "/smallimprint-stock-chart-demo/static/js/2.e40119f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b3150996bc8b41e2a1",
+    "url": "/smallimprint-stock-chart-demo/static/js/main.e34fac83.chunk.js"
+  },
+  {
+    "revision": "48881a3df87bd6f20744",
+    "url": "/smallimprint-stock-chart-demo/static/js/runtime-main.1131e965.js"
+  }
+]);
